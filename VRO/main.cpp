a631f966bc647ee1090f95364a9651ebc8e31cc1
Gokay Titrek <gokaytitrek@hotmail.com>
2018-01-08 13:42:33 +0300

    code clean up

--- VRO/main.cpp
+++ VRO/main.cpp
@@ -12,17 +12,20 @@
 #include <cstdlib>
 #include <math.h>
 #include <float.h>
+#include <fstream>
+#include <time.h>       /* time */
 
 #define NO_OF_CUSTOMER 50
 #define NO_OF_VEHICLES 10
 #define VEHICLE_CAPACITY 80
+#define TABU_COUNT 10
 
 using namespace std;
 
 class Node {
 public:
 	int nodeId, nodeX, nodeY, demand;
-	bool isRouted, isDepot,isValid;
+	bool isRouted, isDepot, isValid;
 
 	//Depot node
 	Node(int coordinateX, int coordinateY);
@@ -102,19 +105,24 @@ class Init {
 public:
 	vector<Node> nodes;
 	vector<Vehicle> vehicles;
+	vector<Vehicle> vehiclesForBestSolution;
 	double distanceMatrix[NO_OF_CUSTOMER + 1][NO_OF_CUSTOMER + 1];// = { { 0 } };
 	int noOfCustomers;
 	int noOfVehicles;
 	int vehicleCap;
-	double cost = 0;
+	int tabuCount;
+	double cost;
+	double bestSolutionCost;
+	vector<double> pastSolutions;
 
 public:
-	Init(int noOfCustomers, int noOfVehicles, int vehicleCap)
+	Init(int noOfCustomers, int noOfVehicles, int vehicleCap, int tabuCount)
 	{
 		this->noOfCustomers = noOfCustomers;
 		this->noOfVehicles = noOfVehicles;
 		this->vehicleCap = vehicleCap;
-
+		this->cost = 0;
+		this->tabuCount = tabuCount;
 		nodes.reserve(this->noOfCustomers);
 		vehicles.reserve(this->noOfVehicles);
 	}
@@ -174,6 +182,7 @@ public:
 	void generateVehicles(){
 		for (int i = 1; i <= this->noOfVehicles; i++) {
 			vehicles.push_back(Vehicle(i));
+			vehiclesForBestSolution.push_back(Vehicle(i));
 		}
 	}
 
@@ -204,16 +213,16 @@ public:
 			double minimumCost = (float)DBL_MAX; //Minimum Cost
 
 			//If route of vehicle is empty, then add first node
-			if (vehicles[vehicleIndex].nodes.empty())
+			if (this->vehicles[vehicleIndex].nodes.empty())
 			{
-				vehicles[vehicleIndex].addNode(nodes[0]);
+				this->vehicles[vehicleIndex].addNode(nodes[0]);
 			}
 
 			for (int i = 1; i <= noOfCustomers; i++) {
 				//If customer has not a root yet
-				if (nodes[i].isRouted == false) {
+				if (this->nodes[i].isRouted == false) {
 					//Check whether there exists enough space for that customer in vehicle
-					if (vehicles[vehicleIndex].checkCapacity(nodes[i].demand)) {
+					if (this->vehicles[vehicleIndex].checkCapacity(nodes[i].demand)) {
 						//Find the distance between vehicle's cureent prosition and customer 
 						CandidateCost = this->distanceMatrix[vehicles[vehicleIndex].currentLocation][i];
 						//If that distance is smaller than found minCost found so far
@@ -222,6 +231,7 @@ public:
 							minimumCost = CandidateCost;
 							CustomerIndex = i;
 							CandCustomer = nodes[i];
+							CandCustomer.isValid = true;
 						}
 
 					}
@@ -231,11 +241,11 @@ public:
 			if (!CandCustomer.isValid)
 			{
 				//That customer does not fit
-				if (vehicleIndex + 1 < sizeof(vehicles)) //We can assign that customer to other cancidate vehicles
+				if (vehicleIndex + 1 < this->noOfVehicles) //We can assign that customer to other cancidate vehicles
 				{
-					if (vehicles[vehicleIndex].currentLocation != 0) {//Finish the route for that vehicle
-						EndCost = this->distanceMatrix[vehicles[vehicleIndex].currentLocation][0]; // Find the distance of that vehicle to depot
-						vehicles[vehicleIndex].addNode(nodes[0]); // Add depot to its route
+					if (this->vehicles[vehicleIndex].currentLocation != 0) {//Finish the route for that vehicle
+						EndCost = this->distanceMatrix[this->vehicles[vehicleIndex].currentLocation][0]; // Find the distance of that vehicle to depot
+						this->vehicles[vehicleIndex].addNode(nodes[0]); // Add depot to its route
 						this->cost += EndCost; //update total cost of greedy solution
 					}
 					vehicleIndex = vehicleIndex + 1; //Choose next vehicle
@@ -249,25 +259,27 @@ public:
 			}
 			else
 			{
-				vehicles[vehicleIndex].addNode(CandCustomer);//Add this candidate customer to the vehicle's path
+				this->vehicles[vehicleIndex].addNode(CandCustomer);//Add this candidate customer to the vehicle's path
 				nodes[CustomerIndex].isRouted = true;
 				this->cost += minimumCost; //ncrease the total cost for this greedySolution
 			}
 
-			// Find the distance of that vehicle to depot
-			EndCost = this->distanceMatrix[vehicles[vehicleIndex].currentLocation][0];
-			// Add depot to its route
-			vehicles[vehicleIndex].addNode(nodes[0]);
-			//update total cost of greedy solution
-			this->cost += EndCost;
+
 
 		}
+
+		// Find the distance of that vehicle to depot
+		EndCost = this->distanceMatrix[this->vehicles[vehicleIndex].currentLocation][0];
+		// Add depot to its route
+		vehicles[vehicleIndex].addNode(nodes[0]);
+		//update total cost of greedy solution
+		this->cost += EndCost;
 
 	}
 
 	bool CheckUnassignedCustomerExists()
 	{
-		for (int i = 1; i < sizeof(nodes); i++)
+		for (int i = 1; i < this->noOfCustomers; i++)
 		{
 			if (!nodes[i].isRouted)
 				return true;
@@ -275,25 +287,220 @@ public:
 		return false;
 	}
 
-	void PrintSolution(string name)//Print Solution In console
+	void TabuSearchSolutionForVRP()
+	{
+		std::vector<Node> previousRoute;
+		std::vector<Node> destinationRoute;
+
+		int nodeDemand = 0;
+
+		int vehicleIndexSource, vehicleIndexDestination;
+
+		double BestNCost, NeighboorCost;
+
+		int SwapIndexA = -1, SwapIndexB = -1, SwapRouteFrom = -1, SwapRouteTo = -1;
+
+		int MAX_ITERATIONS = 200;
+		int iteration_number = 0;
+
+		int DimensionCustomer = NO_OF_CUSTOMER + 1;
+		vector<vector<int> > TABU_Matrix;
+
+		// Set up sizes. (HEIGHT x WIDTH)
+		TABU_Matrix.resize(DimensionCustomer + 1);
+		for (int i = 0; i < DimensionCustomer + 1; ++i)
+			TABU_Matrix[i].resize(DimensionCustomer + 1);
+
+		this->bestSolutionCost = this->cost; //Initial Solution cost
+
+		bool Termination = false;
+
+		while (!Termination) {
+			iteration_number++;
+			BestNCost = DBL_MAX;
+
+			for (vehicleIndexSource = 0; vehicleIndexSource < NO_OF_VEHICLES; vehicleIndexSource++) {
+				previousRoute = this->vehicles[vehicleIndexSource].nodes;
+
+				int RouteFromLength = previousRoute.size();
+
+				for (int i = 1; i < RouteFromLength - 1; i++) { //Not possible to move depot!
+					for (vehicleIndexDestination = 0; vehicleIndexDestination < NO_OF_VEHICLES; vehicleIndexDestination++) {
+						destinationRoute = this->vehicles[vehicleIndexDestination].nodes;
+						int RouteTolength = destinationRoute.size();
+						for (int j = 0; (j < RouteTolength - 1); j++) { //Not possible to move after last Depot!
+
+							nodeDemand = previousRoute[i].demand;
+
+							if ((vehicleIndexSource == vehicleIndexDestination) || this->vehicles[vehicleIndexDestination].checkCapacity(nodeDemand)) {
+								//If we assign to a different route check capacity constrains
+								//if in the new route is the same no need to check for capacity
+
+								if (((vehicleIndexSource == vehicleIndexDestination) && ((j == i) || (j == i - 1))) == false) // Not a move that Changes solution cost
+								{
+
+									double MinusCost1 = this->distanceMatrix[previousRoute[i - 1].nodeId][previousRoute[i].nodeId];
+									double MinusCost2 = this->distanceMatrix[previousRoute[i].nodeId][previousRoute[i + 1].nodeId];
+									double MinusCost3 = this->distanceMatrix[destinationRoute[j].nodeId][destinationRoute[j + 1].nodeId];
+
+									double AddedCost1 = this->distanceMatrix[previousRoute[i - 1].nodeId][previousRoute[i + 1].nodeId];
+									double AddedCost2 = this->distanceMatrix[destinationRoute[j].nodeId][previousRoute[i].nodeId];
+									double AddedCost3 = this->distanceMatrix[previousRoute[i].nodeId][destinationRoute[j + 1].nodeId];
+
+									//Check if the move is a Tabu! - If it is Tabu break
+									if ((TABU_Matrix[previousRoute[i - 1].nodeId][previousRoute[i + 1].nodeId] != 0)
+										|| (TABU_Matrix[destinationRoute[j].nodeId][previousRoute[i].nodeId] != 0)
+										|| (TABU_Matrix[previousRoute[i].nodeId][destinationRoute[j + 1].nodeId] != 0)) {
+										break;
+									}
+
+									NeighboorCost = AddedCost1 + AddedCost2 + AddedCost3
+										- MinusCost1 - MinusCost2 - MinusCost3;
+
+									if (NeighboorCost < BestNCost) {
+										BestNCost = NeighboorCost;
+										SwapIndexA = i;
+										SwapIndexB = j;
+										SwapRouteFrom = vehicleIndexSource;
+										SwapRouteTo = vehicleIndexDestination;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			for (int o = 0; o < NO_OF_CUSTOMER + 2; o++) {
+				for (int p = 0; p < NO_OF_CUSTOMER + 2; p++) {
+					if (TABU_Matrix[o][p] > 0)
+					{
+						TABU_Matrix[o][p]--;
+					}
+				}
+			}
+
+			previousRoute = this->vehicles[SwapRouteFrom].nodes;
+			destinationRoute = this->vehicles[SwapRouteTo].nodes;
+			this->vehicles[SwapRouteFrom].nodes.clear();
+			this->vehicles[SwapRouteTo].nodes.clear();
+
+			Node SwapNode = previousRoute[SwapIndexA];
+
+			int NodeIDBefore = previousRoute[SwapIndexA - 1].nodeId;
+			int NodeIDAfter = previousRoute[SwapIndexA + 1].nodeId;
+			int NodeID_F = destinationRoute[SwapIndexB].nodeId;
+			int NodeID_G = destinationRoute[SwapIndexB + 1].nodeId;
+
+
+			/* initialize random seed: */
+			srand(time(NULL));
+
+
+			int RendomDelay1 = rand() % 5;
+			int RendomDelay2 = rand() % 5;
+			int RendomDelay3 = rand() % 5;
+
+			TABU_Matrix[NodeIDBefore][SwapNode.nodeId] = this->tabuCount + RendomDelay1;
+			TABU_Matrix[SwapNode.nodeId][NodeIDAfter] = this->tabuCount + RendomDelay2;
+			TABU_Matrix[NodeID_F][NodeID_G] = this->tabuCount + RendomDelay3;
+
+			previousRoute.erase(previousRoute.begin() + SwapIndexA - 1);
+
+			if (SwapRouteFrom == SwapRouteTo) {
+				if (SwapIndexA < SwapIndexB) {
+					destinationRoute.insert(destinationRoute.begin() + SwapIndexB - 1, SwapNode);
+				}
+				else {
+					destinationRoute.insert(destinationRoute.begin() + SwapIndexB, SwapNode);
+				}
+			}
+			else
+			{
+				destinationRoute.insert(destinationRoute.begin() + SwapIndexB, SwapNode);
+			}
+
+			this->vehicles[SwapRouteFrom].nodes = previousRoute;
+			this->vehicles[SwapRouteFrom].load -= nodeDemand;
+
+			this->vehicles[SwapRouteTo].nodes = destinationRoute;
+			this->vehicles[SwapRouteTo].load += nodeDemand;
+
+
+			pastSolutions.push_back(this->cost);
+
+			this->cost += BestNCost;
+
+			if (this->cost <   bestSolutionCost)
+			{
+				SaveBestSolution();
+			}
+
+			if (iteration_number == MAX_ITERATIONS)
+			{
+				Termination = true;
+			}
+
+		}
+		for (int i = 0; i < this->noOfVehicles; i++)
+		{
+			vehicles[i] = vehiclesForBestSolution[i];
+		}
+
+		this->cost = bestSolutionCost;
+
+
+		try{
+			ofstream myfile;
+			myfile.open("PastSol.txt");
+			for (int i = 0; i< pastSolutions.size(); i++){
+				myfile << pastSolutions[i] << "\t";
+			}
+			myfile.close();
+		}
+		catch (...) {
+			cout << "Eception occured";
+		}
+	}
+
+	void SaveBestSolution()
+	{
+		bestSolutionCost = this->cost;
+		for (int j = 0; j < NO_OF_VEHICLES; j++)
+		{
+			vehiclesForBestSolution[j].nodes.clear();
+			if (!this->vehicles[j].nodes.empty())
+			{
+				int RouteSize = this->vehicles[j].nodes.size();
+				for (int k = 0; k < RouteSize; k++) {
+					Node n = this->vehicles[j].nodes[k];
+					vehiclesForBestSolution[j].nodes.push_back(n);
+				}
+			}
+		}
+	};
+
+
+	void PrintSolution(char* name)//Print Solution In console
 	{
 		cout << "=========================================================" << endl;
-		printf("Solution name is : %s\n", name);
+		cout << "Solution is : ";
+		std::cout << name << endl;
 
 		for (int j = 0; j < noOfVehicles; j++)
 		{
-			if (!vehicles[j].nodes.empty())
+			if (!this->vehicles[j].nodes.empty())
 			{
 				cout << "Vehicle " << j << ":";
-				int routeSize = vehicles[j].nodes.size();
+				int routeSize = this->vehicles[j].nodes.size();
 				for (int e = 0; e < routeSize; e++) {
 					if (e == routeSize - 1)
 					{
-						cout << vehicles[j].nodes.at(e).nodeId;
+						cout << this->vehicles[j].nodes.at(e).nodeId;
 					}
 					else
 					{
-						cout << vehicles[j].nodes.at(e).nodeId << "->";
+						cout << this->vehicles[j].nodes.at(e).nodeId << "->";
 					}
 				}
 				cout << endl;
@@ -307,7 +514,7 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "Vehicle routing optimization" << endl;
 
-	Init init(NO_OF_CUSTOMER, NO_OF_VEHICLES, VEHICLE_CAPACITY);
+	Init init(NO_OF_CUSTOMER, NO_OF_VEHICLES, VEHICLE_CAPACITY, TABU_COUNT);
 	init.generateNodes();
 	init.printNodes();
 	init.calculateDistanceMatrix();
@@ -319,8 +526,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	init.GreedySolutionForVRP();
 	init.PrintSolution("Greedy Solution");
 
+	init.TabuSearchSolutionForVRP();
+	init.PrintSolution("Tabu Search Solution");
 
 
 	return 0;
 }
+
 
